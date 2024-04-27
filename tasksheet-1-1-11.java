7cdbdd303a4882b44c6c434e2bd14eeb9a1e15de
J29est <164187278+J29est@users.noreply.github.com>
2024-04-27 09:19:00 +0000

    Done

---
+++ tasksheet-1-1-11.java
@@ -0,0 +1,15 @@
+ class vehicle{ 
+   public static void main(String[] args) {
+    
+      String Make;}
+   public String Name(){
+      return "volvo"; 
+   }
+String model;
+public String getName(){
+   return "C40SUV";
+
+
+}
+ }
+   
