7604d7757fac9412af38fe2c06d44079cc982864
J29est <164187278+J29est@users.noreply.github.com>
2024-04-27 09:44:20 +0000

    Done

---
+++ task3.java
@@ -0,0 +1,9 @@
+public class task3 {
+    public static void main(String[] args) {
+        int check_number = 10;
+        String message=new String();
+        for (int i = 1; i <= check_number; i++){
+            System.out.println(message =(i%2==0) ? i+" is even number" : i+" is odd number");
+        }
+    }
+}
