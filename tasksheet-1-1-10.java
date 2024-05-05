74025c55096e84d507ad4892577fadbca307a6cb
J29est <164187278+J29est@users.noreply.github.com>
2024-05-05 11:39:31 +0000

    Done

--- tasksheet-1-1-10.java
+++ tasksheet-1-1-10.java
@@ -1,3 +1,23 @@
 class tasksheet10{
-    
+   public static void main(String[] args) {
+    Book book1 = new Book("Harry Potter", "J.K. Rowling", 2003, 23.45);
+    Book book2 = new Book("The Lord of the Rings", "J.R.R. Tolkien", 2006, 36.64);
+    Book book3 = new Book("The Twilight Saga", "Stephenie Meyer", 2007, 19.99);
+    System.out.println("Book 1:\nTitle: "+book1.title+"\nAuthor: "+book1.author+"\nYear Published: "+book1.yearPublished+"\nPrice: $"+book1.price+"\n ");
+    System.out.println("Book 2:\nTitle: "+book2.title+"\nAuthor: "+book2.author+"\nYear Published: "+book2.yearPublished+"\nPrice: $"+book2.price+"\n ");
+    System.out.println("Book 3:\nTitle: "+book3.title+"\nAuthor: "+book3.author+"\nYear Published: "+book3.yearPublished+"\nPrice: $"+book3.price+"\n ");
+   } 
+}
+class Book {
+    String title;
+    String author;
+    int yearPublished;
+    double price;
+
+    Book(String title, String author, int yearPublished, double price){
+        this. title = title;
+        this. author = author;
+        this. yearPublished = yearPublished;
+        this. price = price;
+    }
 }
--- tasksheet-1-1-11.java
+++ tasksheet-1-1-11.java
@@ -1,8 +1,19 @@
- class vehicle{ 
+ class Car extends Vehicle{
+   int numberOfDoors; 
+   void displayDetails(){
+      System.out.println("Car Details: \nMake: "+this.Make+"
+      \nModel: "this.Model+"
+      \nYear: "+this.year+"
+      \nNumber of doors: "this.numberOfDoors);
+   }
+   Car(String Make,String  model,int year,int numberOfDoors) {
+    super();
+
+   }
    public static void main(String[] args) {
     
-      String Make;}
-   public String Name(){
+      private String Make;
+      private String  model;
       return "volvo"; 
    }
 String model;
