f07d37f888b7076fd09c8a0454a66e114b667787
J29est <164187278+J29est@users.noreply.github.com>
2024-09-29 06:49:46 +0000

    done

--- BankingApp.java
+++ BankingApp.java
@@ -1,7 +1,8 @@
 import java.util.*;
 public class BankingApp{
-Scanner scanner = new Scanner (System.in);
-Bank bank = new bank();
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner (System.in);
+    Bank bank = new Bank();
 
 
 System.out.println("Welcome to the Banking App!");
@@ -17,10 +18,78 @@ while (!loggedIn) {
 
     loggedInUser = bank.logIn(userId, pin);
     if (loggedInUser != null) {
-        System.out.println("Invalid user ID or PIN. Please try again");
+        loggedIn = true;
+        System.out.println("Successful log in!");
+    }
+    else{
+        System.out.println("Invalid user ID or PIN. Please try again.");
     }
 }
 
+boolean running = true;
+while (running) {
+    System.out.println("\n1. Check Balance");
+    System.out.println("2. Deposit");
+    System.out.println("3. Express Send");
+    System.out.println("4. Quit");
+    System.out.println("\nSelect an option");
+    int option = scanner.nextInt();
+
+    switch (option){
+        case 1: 
+            System.out.println("Current balance: " + loggedInUser.getBalance());
+            break;
+        case 2: 
+            System.out.println("Enter amount to deposit: ");
+            double amountToAdd = scanner.nextDouble();
+            loggedInUser.deposit(amountToAdd);
+            System.out.println("New balance: " + loggedInUser.getBalance());
+            break;
+        case 3: 
+            System.out.println("Enter recipient's user ID:");
+            int recipientId = scanner.nextInt();
+            System.out.println("Enter amount to send: ");
+            double amountToSend = scanner.nextDouble();
+            boolean success = loggedInUser.expressSend(bank, recipientId, amountToSend);
+            if (success){
+            System.out.println("Sent successfully!");
+            } else {
+            System.out.println("Send failed. Check recipient ID or balance.");
+            }
+            break;
+        case 4: 
+            running = false; 
+            System.out.println("Quitting...");
+            break;
+        default:
+            System.out.println("Invalid option. Please try again.");
+            break;
+        }
+    }
+    scanner.close();
+    }
+}
+
+
+class Bank{
+    private Map<Integer, User> users = new HashMap<>();
+    public Bank(){
+        users.put(234544, new User (234544, 2424, "James Adams", 50000));
+        users.put(678901, new User (678901, 7777,"Arthur Lenny", 45000));
+        users.put(432187, new User(432187, 8051, "Mark Zuckerberg", 1000000));
+    }
+
+    public User logIn (int userId, int pin){
+        User user = users.get(userId);
+        if (user != null && user.getPin() == pin){
+            return user;
+        }
+    
+    return null;  
+}
+public User getUser(int userId){
+    return users.get(userId);
+}
 }
 class User {
     private int id;
@@ -29,7 +98,7 @@ class User {
     private double balance;
      
 
-    public User (int id, int pin, String name, duoble balance){
+    public User (int id, int pin, String name, double balance){
         this.id = id;
         this.pin = pin;
         this.name = name;
@@ -41,17 +110,31 @@ public int getPin(){
     public double getBalance(){
    return balance;
    }
-   public void cashIn(doudle amount)
-   if (amount > 0){
-    balance +=amount;
-
-   }
-   else {
-    System.out.println(" Invalid amount.");
-   }
+   public void deposit(double amount){
+    if (amount > 0){
+        balance +=amount;
+        }
+    else {
+        System.out.println("Invalid amount.");
+        }
     }
 
-
+    public boolean expressSend (Bank bank, int recipientId, double amount){
+        if (amount <=0){
+            System.out.println("Invalid amount.");
+            return false;
+        }
+        if (balance < amount) {
+            System.out.println("insufficient balance.");
+            return false;
+        }
+User recipient = bank.getUser (recipientId);
+if (recipient == null){
+    System.out.println("Recipient does not exist.");
+    return false;
 }
-
+  balance -= amount;
+  recipient.deposit(amount);
+  return true;
+}
 }
